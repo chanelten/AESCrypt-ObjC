c6b34a74031ecaa88100cc10348c3fca1cbb7c19
Anthony Weiss <anthony@weissazool.com>
2015-06-02 17:51:08 +0900

    Replace static framework project with dynamic framework project.

---
+++ AESCrypt/AESCrypt-Main.h
@@ -0,0 +1,19 @@
+//
+//  AESCrypt.h
+//  AESCrypt
+//
+//  Created by Anthony Weiss on 6/2/15.
+//  Copyright (c) 2015 TenTen Technologies Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+/*
+//! Project version number for AESCrypt.
+FOUNDATION_EXPORT double AESCryptVersionNumber;
+
+//! Project version string for AESCrypt.
+FOUNDATION_EXPORT const unsigned char AESCryptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AESCrypt/PublicHeader.h>
+*/
+
